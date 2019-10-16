fb009f074de31be2c33b3a7e385da0c2dadb5c3e
shcfighter <shc_fighter@163.com>
2019-10-16 17:27:24 +0800

    log

--- src/main/java/com/ecit/auth/impl/ShopUserSessionHandlerImpl.java
+++ src/main/java/com/ecit/auth/impl/ShopUserSessionHandlerImpl.java
@@ -6,12 +6,16 @@
 package com.ecit.auth.impl;
 
 import com.ecit.auth.ShopUserSessionHandler;
+import com.ecit.common.constants.Constants;
 import com.ecit.common.db.JdbcRxRepositoryWrapper;
 import com.ecit.common.result.ResultItems;
+import com.ecit.shop.constants.UserSql;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,7 +32,32 @@ public class ShopUserSessionHandlerImpl extends JdbcRxRepositoryWrapper implemen
     public void handle(RoutingContext routingContext) {
         String token = routingContext.request().getHeader("token");
         LOGGER.info("url: {}, token: {}", routingContext.request().uri(), token);
-        Future<JsonObject> future = this.getSession(token);
+        //Future<JsonObject> future = this.getSession(token);
+        Future<JsonObject> future = Future.future();
+        redisClient.hget(Constants.VERTX_WEB_SESSION, token, handler -> {
+            if (handler.succeeded()) {
+                String user = handler.result();
+                LOGGER.info("ShopUserSessionHandlerImpl redis user: {}", user);
+                if (StringUtils.isEmpty(user)) {
+                    this.retrieveOne(new JsonArray().add(token), UserSql.SELECT_BY_TOKEN_SQL)
+                            .subscribe(future::complete, future::fail);
+                    future.compose(u -> {
+                        LOGGER.info("ShopUserSessionHandlerImpl db user: {}", u::encodePrettily);
+                        this.setSession(token, u);
+                        return Future.succeededFuture(u);
+                    });
+                }
+                future.complete(new JsonObject(user));
+            } else {
+                LOGGER.info("ShopUserSessionHandlerImpl redis query token error:", handler.cause());
+                this.retrieveOne(new JsonArray().add(token), UserSql.SELECT_BY_TOKEN_SQL)
+                        .subscribe(future::complete, future::fail);
+                future.compose(u -> {
+                    this.setSession(token, u);
+                    return Future.succeededFuture(u);
+                });
+            }
+        });
         future.compose(user -> {
             LOGGER.info("session user: {}", user);
             if (Objects.isNull(user) || user.isEmpty()) {
